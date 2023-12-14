3d695c0c5bfd5227ce663b75f34604cb2337b74c
Varun <varunrawat343@gmail.com>
2023-12-15 00:50:33 +0530

    cleaned up my code

--- 158B.cpp
+++ 158B.cpp
@@ -22,26 +22,21 @@ void solve() {
     ll a4 = arr[4];
 
     ll ans = 0;
-    ans += a4;
+    
+    ans += a4; // Obv all 4 ones should be added separately
 
-    ll min1or3 = min(a1,a3);
-    ans += min1or3;
+    ans += a3; // Take all 3's
+    a1 -= min(a1,a3); // Now a1 will be this
 
-    a1 -= min1or3;
-    a3 -= min1or3;
+    ans += a2/2; // All 2's must be added divided by 2
 
-    ans += a3;
-
-    ans += a2/2;
-
-    a2 = a2%2;
-
-    if (a2 != 0) {
-        a1 -= 2;
-        a1 = max(a1,0LL);
+    // If any 2 is still left
+    if (a2&1) {
+        a1 = max(a1-2,0LL); // Two 1's will be removed and we take only positive elements
         ans++;
     }
 
+    // Now add the remaining 1's
     ans += a1/4 + (a1%4 != 0);
 
     cout << ans << "\n";
