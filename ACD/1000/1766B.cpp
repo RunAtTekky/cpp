14bbfe440aa3a9e5bf0913634b1030fbfc6c1508
Varun <varunrawat343@gmail.com>
2023-12-28 23:57:00 +0530

    [practise]

--- ACD/1000/1766B.cpp
+++ ACD/1000/1766B.cpp
@@ -26,13 +26,14 @@ void solve() {
     cin >> n;
     string s; cin >> s;
 
-    map<string,int> mp;
+    map<string,int> mp; // {string : index}
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<i; j++) {
-            mp[s.substr(j,i+1-j)]++;
-            if (mp[s.substr(j,i+1-j)] == 2) {YES; return;}
+    for (int i=0; i<n-1; i++) {
+        string sub = s.substr(i,2);
+        if (mp.find(sub) != mp.end()) {
+            if (mp[sub] + 1 < i) {YES; return;}
         }
+        else mp[sub] = i;
     }
     NO;
 
