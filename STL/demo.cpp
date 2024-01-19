11a106d57231783ab63337060076418a8e32b0a0
Varun <varunrawat343@gmail.com>
2024-01-19 23:02:42 +0530

    create copy of vector

--- STL/demo.cpp
+++ STL/demo.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-#include <string>
-
+#include <vector>
 using namespace std;
 
 
+// Program to create a vector copy from another vector.
 int main() {
-    int n; cin >> n;
-    char c; cin >> c;
-    string s; cin >> s;
+  int n; cin >> n;
+  vector<int> vec(n);
+  for (auto &e: vec) cin >> e;
 
-    for (int i=0; i<n; i++) {
-        if (s[i] == c) {
-            cout << i << "s";
-        }
-        else {
-            cout << i << "n";
-        }
-    }
+  vector<int> vec2;
+  vec2 = vec;
+
+  vec2[0] = 8;
+
+  for (auto &e: vec) cout << e << " ";
+  cout << "\n";
+  for (auto &e: vec2) cout << e << " ";
+  cout << "\n";
+
 }
