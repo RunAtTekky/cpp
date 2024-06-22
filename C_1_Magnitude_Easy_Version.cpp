8921ca3b5279c86b646fabcc776134bd32da3a56
Varun <varunrawat343@gmail.com>
2024-06-22 23:45:46 +0530

    template update

--- C_1_Magnitude_Easy_Version.cpp
+++ C_1_Magnitude_Easy_Version.cpp
@@ -2,8 +2,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
+void solve() {
+  ll n; cin >> n;
+  vector<ll> a(n); for (auto &e: a) cin >> e;
+
+  ll maxNegative = 0, sum = 0;
+  for (auto &num : a) {
+    sum += num;
+    maxNegative = min(maxNegative, sum);
+  }
+
+  /*
+   When sum is the most negative
+   at that moment we must make choose option 2
+  */
+
+  ll ans = sum - 2*maxNegative;
+
+  cout << ans << "\n";
+
+}
+
 int main() {
   cin.sync_with_stdio(0);
   cin.tie(0);
+
+  int t; cin >> t;
+  while (t--) solve();
   
 }
--- Templates/basic.cpp
+++ Templates/basic.cpp
@@ -2,8 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+  int n; cin >> n;
+
+}
+
 int main() {
-  cin.sync_with_stdio(0);
-  cin.tie(0);
-  
+  cin.sync_with_stdio(0); cin.tie(0);
+  int t; cin >> t; while (t--) solve();
 }
